5e7b6e2f9328a15a10de65da93b46433d6b92db6
AtheerAPeter <atheer07713991785@gmail.com>
2020-09-19 18:19:08 +0300

    initial commit

---
+++ components/todoform.jsx
@@ -0,0 +1,55 @@
+import { Input, Button, Row, Col } from "antd";
+import { PlusCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import uuid from "react-uuid";
+
+const TodoForm = ({ todo, todos, setTodos, setTodo }) => {
+  return (
+    <div className="form">
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (todo == "") return;
+          const newItem = [...todos, { name: todo, id: uuid() }];
+          setTodos(newItem);
+          localStorage.setItem("todos", JSON.stringify(newItem));
+          setTodo("");
+        }}
+      >
+        <Row gutter={5}>
+          <Col span={3}>
+            <Button
+              onClick={() => {
+                setTodos([]);
+                localStorage.setItem("todos", "");
+              }}
+              style={{ width: "100%" }}
+              type="primary"
+              icon={<DeleteOutlined />}
+              danger
+            >
+              All
+            </Button>
+          </Col>
+          <Col span={18}>
+            <Input
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+              placeholder="write something"
+            />
+          </Col>
+          <Col span={3}>
+            <Button
+              type="submit"
+              style={{ width: "100%" }}
+              icon={<PlusCircleOutlined />}
+              type="primary"
+            ></Button>
+          </Col>
+        </Row>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
